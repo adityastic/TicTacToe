ca3060d7cc1bae5b24183bd00442a25ad6c7ef2c
adityagupta <adityaofficialgupta@gmail.com>
2019-11-06 22:31:11 +0000

    chore: updated client ip for application

--- TicTacToe/src/com/tictactoe/client/TicTacToeClient.java
+++ TicTacToe/src/com/tictactoe/client/TicTacToeClient.java
@@ -84,7 +84,7 @@ public class TicTacToeClient {
 	}
 
 	private void initializeClient() throws MalformedURLException, RemoteException, NotBoundException {
-		client = new ClientImpl((ServerInterface) Naming.lookup("rmi://192.168.43.52/tictacToeServer"), new OnMessageListener() {
+		client = new ClientImpl((ServerInterface) Naming.lookup("rmi:///tictacToeServer"), new OnMessageListener() {
 
 			@Override
 			public void showMessage(String msg) {
